daf0a59f5a1ea471a69986baa9cb711f4cd1064c
Marco_Wedemeyer <41507549+MarcoWedemeyer@users.noreply.github.com>
2021-03-27 10:41:50 +0100

    Initial web framework set up

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
+
+## This will run the file locally when executed directly.
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
