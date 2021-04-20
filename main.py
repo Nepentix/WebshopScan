bf70b2d7028765b895c63759561b0de1b448b6dd
Marco_Wedemeyer <41507549+MarcoWedemeyer@users.noreply.github.com>
2021-04-20 19:09:55 +0200

    redirection framework

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, redirect
+# import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="static")
 
-@app.route("/")
+@app.route("/",  methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        form_data = request.form
+        site = form_data["url"]
+        site = site.replace("http://","").replace("https://","").replace("www.","")
+        return redirect(f"/result/{site}", code=302)
+
+@app.route("/result/")
+def result():
+    return redirect("/", code=302)
+
+@app.route("/result/<site>")
+def scanning(site):
+    return f"Result for {site}"
 
 ## This will run the file locally when executed directly.
 if __name__ == "__main__":
