49ccc9712046174d1b6b0550ce4d512d63e6af47
Thomas van der Wolde <thomas@nepentix.com>
2021-04-29 23:17:36 +0200

    Start.py and LinkedIn

---
+++ start.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("cd C:\xampp\htdocs\WebshopScan\venv\Scripts")
+os.system("Activate")
+os.system("cd C:\xampp\htdocs\WebshopScan")
+os.system("pip install -r requirements.txt")
+os.system("py main.py")
